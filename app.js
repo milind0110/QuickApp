77469958751ae880afed93774a476511743d4aa5
milind0110 <milindg0110@gmail.com>
2022-11-05 14:19:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// const cors = require("cors");
+const express = require("express");
+
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);  
+
+app.use(express.static("public"));
+
+
+const user = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        user[socket.id] = name;
+        socket.broadcast.emit("user-joined",name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit("receive", {message : message, name : user[socket.id]});
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit("leave", user[socket.id]);
+        delete user[socket.id];
+    });
+})
+
+
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000,() => {
+    console.log("Listening to port 3000");
+});
---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+// const socket = io();
+
+// const form = document.getElementById("send-container");
+
+// const messageInput = document.getElementById("messageInp");
+
+// const messageContainer = document.querySelector(".container");
+
+// const name = prompt("Enter your name to join");
+// alert("Hello");
+// socket.emit('new-user-joined', name);
